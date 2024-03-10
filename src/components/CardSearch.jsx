81bb14394cad3593343c0acc3f50b845cbd12d79
YOA\yoaas <syogaadi75@gmail.com>
2024-03-10 14:06:14 +0700

    add dark theme

--- src/components/CardSearch.jsx
+++ src/components/CardSearch.jsx
@@ -51,17 +51,22 @@ function CardSearch({ data }) {
     <motion.div>
       <motion.div className="flex items-center text-xs">
         <motion.div
-          className={`bg-primary py-1 px-2 lg:py-2 lg:px-3 rounded-tl-md lg:rounded-tl-lg flex items-center text-yellow-400`}
+          className={`bg-primary dark:bg-light py-1 px-2 lg:py-2 lg:px-3 rounded-tl-md lg:rounded-tl-lg flex items-center font-semibold text-yellow-500`}
         >
           <StarIcon className="w-4 mr-1" />
           <span>{rating}</span>
         </motion.div>
-        <motion.div className="text-primary py-1 px-2 lg:py-2 lg:px-3 bg-primary/10 rounded-tr-md lg:rounded-tr-lg w-full capitalize">
+        <motion.div className="text-primary py-1 px-2 lg:py-2 lg:px-3 bg-primary/10 dark:bg-light/10 dark:text-light rounded-tr-md lg:rounded-tr-lg w-full">
           {status}
         </motion.div>
       </motion.div>
       <motion.div className="recent-item" whileHover="hover" onClick={() => goToDetail(slug)}>
-        <motion.img variants={imgMotion} className="z-1 object-cover" src={cover} alt="cover" />
+        <motion.img
+          variants={imgMotion}
+          className="z-1 object-cover two-linear-mask-image"
+          src={cover}
+          alt="cover"
+        />
         <motion.div variants={hideTextMotion} className="title-container">
           <motion.p className="truncate-text-2">{title}</motion.p>
         </motion.div>
@@ -75,7 +80,7 @@ function CardSearch({ data }) {
         </motion.div>
       </motion.div>
       <motion.div className="flex items-center text-xs">
-        <motion.div className="text-primary h-[2.42rem] lg:h-[2.7rem] py-1 px-2 lg:py-2 lg:px-3 bg-primary/10 rounded-b-md lg:rounded-b-lg w-[150px] lg:w-[220px] capitalize flex flex-wrap gap-2 truncate-text-2">
+        <motion.div className="text-primary dark:text-secondary h-[2.42rem] lg:h-[2.7rem] py-1 px-2 lg:py-2 lg:px-3  rounded-b-md lg:rounded-b-lg w-[150px] lg:w-[220px] capitalize flex flex-wrap gap-2 truncate-text-2">
           {genres?.map((genre, i) => (
             <motion.span className="font-semibold">
               {genre.text}
